51f1ac79f9bda85874abfe9cfded2dfcc017ac48
Emiliano Menéndez <emystein@gmail.com>
2022-12-01 07:44:15 -0300

    Quit imwheel when change button values to 0

--- utils.js
+++ utils.js
@@ -11,12 +11,15 @@ const ExtensionUtils = imports.misc.extensionUtils;
  */
 function setServiceMode(value) {
     const confFile = Gio.File.parse_name('~/.imwheelrc');
+    const imWheelProgramWheel = 'imwheel --kill -b "4 5"';
+    const imWheelQuit = 'imwheel --kill --quit';
 
     const text = `".*"\nNone,      Up,   Button4, ${value}\nNone,      Down, Button5, ${value}\nControl_L, Up,   Control_L|Button4\nControl_L, Down, Control_L|Button5\nShift_L,   Up,   Shift_L|Button4\nShift_L,   Down, Shift_L|Button5`;
     const [success, tag] = confFile.replace_contents(text, null, false,  Gio.FileCreateFlags.REPLACE_DESTINATION, null);
 
     if (success) {
-        GLib.spawn_command_line_async('imwheel -kill -b "4 5"');
+        imWheelCommand = value != '0' ? imWheelProgramWheel : imWheelQuit;
+        GLib.spawn_command_line_async(imWheelCommand);
     }
 
     return success;
